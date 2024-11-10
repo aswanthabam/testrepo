de48b0caadfa65bd473d702f1e647fe73126a434
Aswanth Vc <aswanth.abam@gmail.com>
2024-11-11 01:41:16 +0530

    dd

--- scripts/analyze.js
+++ scripts/analyze.js
@@ -177,13 +177,14 @@ async function getPRAuthorStats(github, context) {
     repo = context.repo.repo;
     issue_number = context.issue.number;
     body = JSON.stringify(data, null, 4);
+    console.log("Data:", data);
     await octokit.request(
       `POST /repos/${owner}/${repo}/issues/${issue_number}/comments`,
       {
         owner,
         repo,
         issue_number,
-        body,
+        body: "test",
         headers: {
           "X-GitHub-Api-Version": "2022-11-28",
         },
